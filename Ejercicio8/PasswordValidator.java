994f684e1b6f5fb2d523a481477adbef3edabb4f
Evelyn <evemh19@gmail.com>
2026-01-21 21:23:46 -0600

    Ejercicio 8 corregido

--- Ejercicio8/PasswordValidator.java
+++ Ejercicio8/PasswordValidator.java
@@ -6,20 +6,37 @@ public class PasswordValidator {
     public static void main(String [] args){
         String password = IOManager.readString("Provide a password:");
         
-        //Corregir 
         while(true){
-            if(password.length() < 8){
-                IOManager.print("Password must be at least 8 characters long.");
-            } else if (!password.matches(".*[A-Z].*")) { 
+           boolean isValid = true;
+
+           if(!isMinLength(password)){
+               IOManager.print("Password must be at least 8 characters long.");
+                isValid = false;
+           } 
+
+           if(!itHasUppercase(password)){
                 IOManager.print("Password must contain at least one uppercase letter.");
-            } else {
-                IOManager.print("Password is valid.");
+                isValid = false;
+           }
+
+           if(isValid) {
+                IOManager.print("Password is valid");
                 break;
-            }
+           }
+
             password = IOManager.readString("Provide a password:");
 
         }
 
         
     }
+    public static boolean isMinLength(String password){
+        return password.length() >= 8;
+    }
+
+    public static boolean itHasUppercase(String password){
+        return password.matches(".*[A-Z].*");
+    }
 }
+
+
